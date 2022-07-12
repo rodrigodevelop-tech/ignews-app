94e2800ab6c2fe607c70cb8ca76a0f84b5519ce4
rodrigodevelop-tech <digoalves12@hotmail.com>
2022-07-12 10:39:08 -0300

    added route when user is not logged in or signed up

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -69,7 +69,7 @@ export const getServerSideProps: GetServerSideProps = async ({
   if (!session?.activeSubscription) {
     return {
       redirect: {
-        destination: "/",
+        destination: `/posts/preview/${slug}`,
         permanent: false,
       },
     };
